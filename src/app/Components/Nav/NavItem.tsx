464e37a2b6677fc33e9787145f078cee9b30b085
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-05-27 12:42:29 +0300

    create Navbar

---
+++ src/app/Components/Nav/NavItem.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+
+interface NavItemProps {
+  href: string
+  title: string
+  icon?: React.ReactNode
+  active?: boolean
+}
+
+const NavItem = ({ href, title, icon, active }: NavItemProps) => {
+  return (
+    <li>
+      <Link
+        href={href}
+        className={`flex gap-4 justify-between items-center p-4 py-2 hover:bg-emerald-900 rounded-lg transition-all
+        ${active ? 'bg-emerald-900 font-black' : ''}
+        `}
+      >
+        {icon && <span className=''>{icon}</span>}
+        {title}
+      </Link>
+    </li>
+  )
+}
+
+export default NavItem
---
+++ src/app/Components/Nav/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import NavItem from './NavItem'
+import { FaBars, FaHamburger, FaSearch } from 'react-icons/fa'
+import Image from 'next/image'
+import Logo from '../../../../public/Logo'
+
+const Navbar = () => {
+  return (
+    <nav className='h-20 bg-emerald-950 text-white flex items-center'>
+      <div className='container flex justify-between items-center  h-full'>
+        <div className='logo'>
+          <Link
+            href={'/'}
+            className='text-4xl font-black uppercase flex justify-between items-center gap-4'
+          >
+            <Logo />
+          </Link>
+        </div>
+        <ul className='nav-links hidden text-base justify-between items-center gap-4 md:flex'>
+          <NavItem href='/' title='Home' active />
+          <NavItem href='/movies' title='Movies' />
+          <NavItem href='/tv-shows' title='TV Shows' />
+          <NavItem href='/search' title='Search' icon={<FaSearch />} />
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
