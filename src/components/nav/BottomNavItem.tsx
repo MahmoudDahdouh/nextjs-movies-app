19f0bfe15b23ae0bda13cfd8dd69df391654c2d0
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-06-07 15:09:05 +0300

    create Bottom Nav component

---
+++ src/components/nav/BottomNavItem.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import { HiHome } from 'react-icons/hi'
+
+interface BottomNavItemProps {
+  href: string
+  title: string
+  activeIcon?: React.ReactNode
+  icon?: React.ReactNode
+  active?: boolean
+}
+
+const BottomNavItem = ({
+  href,
+  title,
+  activeIcon,
+  icon,
+  active,
+}: BottomNavItemProps) => {
+  return (
+    <Link
+      href={href}
+      className={`flex flex-col flex-1 items-center justify-between gap-1 text-white text-center hover:bg-emerald-900 transition-all rounded-lg p-1
+      ${active ? 'bg-emerald-900 font-black' : ''}
+      `}
+    >
+      <div className='text-2xl'>{active ? activeIcon : icon}</div>
+      {title}
+    </Link>
+  )
+}
+
+export default BottomNavItem
