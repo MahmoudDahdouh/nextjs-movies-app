bc973ba4b269a8c04fb45b09cd518008ac754fa2
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-07-03 21:34:39 +0300

    add image.tmdb.org to next.config - images source

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // add tmdb image url to next image loader domains
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/w500/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
