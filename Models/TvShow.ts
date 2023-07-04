a0b6f81d5b10e2136b920a5f81f2a93e7f4623dc
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-07-04 16:14:39 +0300

    create TvShow model

---
+++ Models/TvShow.ts
@@ -0,0 +1,15 @@
+interface TvShow {
+  id?: number
+  backdrop_path: string
+  first_air_date?: string
+  genre_ids?: number[]
+  origin_country?: string[]
+  name?: string
+  original_language?: string
+  overview?: string
+  popularity?: number
+  original_name?: string
+  poster_path: string
+  vote_average?: number
+  vote_count?: number
+}
