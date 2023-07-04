c6a0e1be4c1856f398efa55e559cc7849a2185fd
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-07-04 16:14:28 +0300

    update Movie model

--- Models/Movie.ts
+++ Models/Movie.ts
@@ -3,8 +3,8 @@ interface Movie {
   original_title?: string
   title?: string
   overview?: string
-  backdrop_path?: string | '/temp-cover.webp'
-  poster_path?: string
+  backdrop_path: string
+  poster_path: string
   release_date?: string
   vote_average?: number
   vote_count?: number
