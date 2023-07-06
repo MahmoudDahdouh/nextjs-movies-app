1769a3dd311796775a0324b8ca72b124da39d1e6
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-07-06 15:16:28 +0300

    add NEXT_PUBLIC to env variables

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
-const accessToken = process.env.API_TOKEN
+const accessToken = process.env.NEXT_PUBLIC_API_TOKEN
 
 const instance = axios.create({
-  baseURL: process.env.API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
