4df92a3bc9ab5c83718cbb3fbabafc3e86fcf2ce
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-07-11 13:13:50 +0300

    Crew Model

---
+++ Models/Crew.ts
@@ -0,0 +1,13 @@
+interface Crew {
+  id?: number
+  adult?: boolean
+  gender: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity?: number
+  profile_path: string
+  credit_id?: string
+  job?: string
+  department?: string
+}
