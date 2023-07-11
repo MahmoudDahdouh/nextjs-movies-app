27dd21b37b4c8e7e629f6361cc0b59e8f4edc9f9
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-07-11 11:46:25 +0300

    utils function - Convert number to hrs and mins

--- src/utils/UtilsFunctions.ts
+++ src/utils/UtilsFunctions.ts
@@ -7,3 +7,10 @@ const USDollar = new Intl.NumberFormat('en-US', {
 export const UsdFormatCurrency = (value: number) => {
   return USDollar.format(value)
 }
+
+// Convert number to hours and minutes
+export const ConvertNumberToHoursAndMinutes = (value: number) => {
+  const hours = Math.floor(value / 60)
+  const minutes = value % 60
+  return `${hours}h ${minutes}m`
+}
