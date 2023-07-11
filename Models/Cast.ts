65c93950d7091733f3343816745ced18872c01d5
MahmoudDahdouh <mahmooddahdooh99@gmail.com>
2023-07-11 13:13:57 +0300

    Cast Model

---
+++ Models/Cast.ts
@@ -0,0 +1,14 @@
+interface Cast {
+  id?: number
+  adult?: boolean
+  gender: number
+  known_for_department: string
+  name: string
+  original_name: string
+  popularity?: number
+  profile_path: string
+  cast_id?: number
+  character?: string
+  credit_id?: string
+  order?: number
+}
